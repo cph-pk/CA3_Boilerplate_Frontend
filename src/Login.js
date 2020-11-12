120a50286f17e4150b351bc35fc8913171649e2d
Per <cph-pk171@cphbusiness.dk>
2020-11-12 17:07:33 +0100

    Altered settings

--- src/Login.js
+++ src/Login.js
@@ -25,13 +25,13 @@ function LogIn({ login }) {
           <Col>
             <h2>Login</h2>
             <Form onChange={onChange} className="mt-4">
-              <Form.Group controlId="loginForm">
+              <Form.Group controlId="username">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="text" id="username" placeholder="Enter username" />
+                <Form.Control type="text" placeholder="Enter username" />
               </Form.Group>
-              <Form.Group controlId="formBasicPassword">
+              <Form.Group controlId="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" id="password" placeholder="Enter password" />
+                <Form.Control type="password" placeholder="Enter password" />
               </Form.Group>
               <Button onClick={performLogin} variant="primary" type="submit">
                 Login
